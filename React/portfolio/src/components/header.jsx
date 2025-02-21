41c89e52dbec6c593e80d390fe346f8004b3d6f7
Raj Vardhan <rajvardaan130@gmail.com>
2025-02-21 15:42:22 +0530

    setting updated

--- React/portfolio/src/components/header.jsx
+++ React/portfolio/src/components/header.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-
 const Header = () => {
   return (
     <>
       <div className="h-18 text-red-500 bg-blue-200 font-bold flex items-center justify-center font-mono text-4xl">
-        I am Full-Stack Developer 
+        I am Full-Stack Developer
       </div>
     </>
   );
