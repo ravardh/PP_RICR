fd4726703ea15858e54046f8434dffcfc8874ebf
Raj Vardhan <rajvardaan130@gmail.com>
2025-02-25 13:18:35 +0530

    middle ware

--- nodeTutorial/sampleExpress/src/server.js
+++ nodeTutorial/sampleExpress/src/server.js
@@ -4,23 +4,38 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/login",(req,res)=>[
-    res.send("Login Done")
-])
+const middleware = (req, res, next) => {
+  console.log(`${req.method}  |  ${req.url}  |  ${new Date().toISOString()}`);
+  next();
+};
 
+app.use(middleware);
 
-app.post("/signup",(req,res)=>{
-    let {nm,unm,pass} = req.body;
+const middleware2 = (req, res, next) => {
+  console.log(`${req.method}  |  ${req.url}  |  ${new Date().toISOString()}`);
+  console.log("I am midddleware 2");
+  next();
+};
 
-    console.log(req.body);
-    console.log("User Login Data");
-    
-    console.log(nm,unm,pass);
-    res.status(400).json({message:"User Created"})
-})
+const middleware3 = (req, res, next) => {
+  console.log(`${req.method}  |  ${req.url}  |  ${new Date().toISOString()}`);
+  console.log("I am midddleware 3");
+  next();
+};
+
+app.get("/login", (req, res) => [res.send("Login Done")]);
+
+app.post("/signup", middleware2, middleware3 , (req, res) => {
+  let { nm, unm, pass } = req.body;
+
+  console.log(req.body);
+  console.log("User Login Data");
+
+  console.log(nm, unm, pass);
+  res.status(200).json({ message: "User Created" });
+});
 
 app.listen(5000, () => {
   console.log("Server is running at 5000");
   console.log("I am backend server");
-  
 });
